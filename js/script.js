081fffa2d80ce7c17247cb73a46ac0bbe5c21740
GameFather <musorschik2008@yandex.ru>
2025-05-04 16:31:50 +0500

    Create

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.about-slider').slick({
+        centerMode: false,
+        centerPadding: '10px',
+        slidesToShow: 1,
+        speed: 1000,
+    });
+});
+
+// $('.center').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 3,
+//     responsive: [
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 arrows: false,
+//                 centerMode: true,
+//                 centerPadding: '40px',
+//                 slidesToShow: 3
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 arrows: false,
+//                 centerMode: true,
+//                 centerPadding: '40px',
+//                 slidesToShow: 1
+//             }
+//         }
+//     ]
+// });
